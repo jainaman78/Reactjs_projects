52b45e9f4973d96c469b1f3ee382bec9d939116a
jainaman78 <jainaman9003@gmail.com>
2023-04-20 22:51:37 +0530

    changes in Textutils project

--- firstproject/src/components/TextForm.js
+++ firstproject/src/components/TextForm.js
@@ -1,8 +1,13 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const handle =()=>{
+    const handleupper =()=>{
         let newtext=text.toUpperCase();
+        settext(newtext);
+
+    }
+    const handlelower =()=>{
+        let newtext=text.toLowerCase();
         settext(newtext);
 
     }
@@ -13,14 +18,24 @@ export default function TextForm(props) {
     const [text, settext] = useState("Enter text Here");
     
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div class="mb-3">
                 <label for="My area" class="form-label">{}</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" value={text} onChange={Handleonchange} rows="10"></textarea>
             </div>
-            <button type="button" class="btn btn-dark" onClick={handle}>Uppercase</button>
+            <button type="button" class="btn btn-dark mx-2" onClick={handleupper}>Uppercase</button>
+            <button type="button" class="btn btn-dark mx-2" onClick={handlelower}>Lowercase</button>
 
         </div>
+        <div className='container my-3'>
+            <h1>Text Summary</h1>
+            <p>{text.split(" ").length} Words {text.length} length</p>
+            <h1>text Preview</h1>
+            <p>{text}</p>
+        </div>
+        </>
+        
     )
 }
